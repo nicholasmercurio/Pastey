b61b63fc89f83c70f39c711f8d44bfe5ddf9a04a
Nicholas Mercurio <nick@nicholasmercur.io>
2019-03-10 19:12:19 -0400

    First commit

---
+++ src/pages/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123,
+        "my_list": [123, 4342, 1252]
+    }
+    return render(request, "about.html", my_context)
---
+++ src/products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title       = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price       = models.DecimalField(max_digits=1000, decimal_places=2)
+    summary     = models.TextField(default='this is cool!')
+    featured    = models.BooleanField(default=True)
